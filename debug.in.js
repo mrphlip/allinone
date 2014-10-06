31cd9028650ccef1167ab666e0f0d38a68ba089e
phlip <git@mrphlip.com>
2014-10-06 17:00:28 +1100

    Remove debug button

--- debug.in.js
+++ debug.in.js
@@ -6,6 +6,7 @@ DebugModule.prototype.init = function init()
 	if (!globals.flashmovie)
 		return;
 	
+	/*
 	// add debug button to settings panel
 	var settingrow = document.createElement('li');
 	globals.modules.settingspane.settingslist.appendChild(settingrow);
@@ -14,11 +15,11 @@ DebugModule.prototype.init = function init()
 	settingbutton.style.width = "100%";
 	settingbutton.addEventListener('click', this.doDebug.bind(this), true);
 	settingrow.appendChild(settingbutton);
-	
+	*/
 };
 DebugModule.prototype.doDebug = function doDebug()
 {
-	utils.downloadWiki("HRWiki", function(t){window.alert(t);}, function(s,t){window.alert("Error: " + s + "\n" + t);});
+	//utils.downloadWiki("HRWiki", function(t){window.alert(t);}, function(s,t){window.alert("Error: " + s + "\n" + t);});
 };
 DebugModule.prototype.updateSettings = function updateSettings()
 {
